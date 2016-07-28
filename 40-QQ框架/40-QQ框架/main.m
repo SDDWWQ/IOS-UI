6eb921ebb199f12b46adec1f683f0412f2b20956
SDD <1058088225@qq.com>
2016-07-28 20:15:02 +0800

    QQ界面框架搭建

---
+++ 40-QQ框架/40-QQ框架/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  40-QQ框架
+//
+//  Created by shadandan on 16/7/28.
+//  Copyright © 2016年 SDD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
